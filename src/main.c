bb494d55d5d2a767278253a06a763e6c15068857
Rexm4 <marcosrex4@gmail.com>
2026-02-22 16:01:40 +0100

    Scanner I2C

--- src/main.c
+++ src/main.c
@@ -1,22 +1,29 @@
+#include <freertos/FreeRTOS.h>
 #include "comms.h"
 #include "pump.h"
+#include "scanneri2c.h"
 #include "sensors.h"
 #include "system.h"
 
 void app_main() {
+
+  while (1) {
+    scanner_scan();
+    vTaskDelay(100);
+  }
 
   system_init();
 
   sensors_init();
 
   for (;;) {
-	sensors_update();
+    sensors_update();
 
-	//..
+    //..
 
-	comms_send();
-	pump_actuate();
+    comms_send();
+    pump_actuate();
 
-	system_sleep();
+    system_sleep();
   }
 }
